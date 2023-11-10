e1674ecda49864b38efd40b7e6c5e68be058e073
MalwareMechanic <malwaremechanic@gmail.com>
2023-11-10 10:26:03 -0500

    Replaced shim path

--- packages/resourcehacker.vm/tools/chocolateyinstall.ps1
+++ packages/resourcehacker.vm/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ Import-Module vm.common -Force -DisableNameChecking
 try {
   $toolName = 'Resource Hacker'
   $category = 'Utilities'
-  $shimPath = '\bin\ResourceHacker.exe'
+  $exePath = '\Resource Hacker\ResourceHacker.exe'
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
-  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
+  $executablePath = Join-Path ${Env:programfiles(x86)} $exePath -Resolve
   Install-BinFile -Name $toolName -Path $executablePath
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
   VM-Assert-Path $shortcut

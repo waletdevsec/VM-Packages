14a34e092e52fdd4359f4dbf31b0e415c8c19ad4
MalwareMechanic <malwaremechanic@gmail.com>
2023-11-10 12:50:53 -0500

    Update to use portable version

--- packages/resourcehacker.vm/tools/chocolateyinstall.ps1
+++ packages/resourcehacker.vm/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ Import-Module vm.common -Force -DisableNameChecking
 try {
   $toolName = 'Resource Hacker'
   $category = 'Utilities'
-  $exePath = '\Resource Hacker\ResourceHacker.exe'
+  $shimPath = '\bin\ResourceHacker.exe'
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
-  $executablePath = Join-Path ${Env:programfiles(x86)} $exePath -Resolve
+  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
   Install-BinFile -Name $toolName -Path $executablePath
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
   VM-Assert-Path $shortcut
